51e407b5937edd82d2ff828453fc5f015f566fca
Maria Temponera <153009940+mariatemp@users.noreply.github.com>
2025-05-06 23:56:58 +0300

    one-way-binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-introduction';
+  name = 'Maria';
+
+  person = {
+    givenName: 'Maria',
+    surName: 'Temponera',
+    age: 34,
+    email: 'chtemp@aueb.gr'
+  }
 }
